49be5745e9303dd2832b90568cf7f5f59fc82dcf
Turchanov Denis <turchanovdenis786@gmail.com>
2022-11-15 18:32:35 +0200

    add socket

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require('express')
-const app = express()
+const cors = require('cors');
+const app = express().use(cors())
 const port = 3000
+
+const server = require('http').Server(app)
+
+const io =require('socket.io')(server, {cors: {origin: "*"}});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('connnect', socket.id)
+    
+})
+
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {io} from 'socket.io-client'
 import './App.css'
 
 function App() {
- 
+ const socket = io('http://localhost:3000')
 
   return (
     <div className="App">
